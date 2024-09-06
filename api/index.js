057b47f009449a2c2b69d487fbe6e109f6539414
Nabeel Rehman <nabeelrehman6057@gmail.com>
2024-09-06 12:25:41 -0700

    first commit

--- api/index.js
+++ api/index.js
@@ -1,34 +1,5 @@
-import express from "express";
-import dotenv from "dotenv";
-import cors from "cors";
-import { errorMiddleware } from "./middlewares/error.js";
-import reservationRouter from "./routes/reservationRoute.js";
-import { dbConnection } from "./database/dbConnection.js";
-import  contactRouter  from "./routes/contactRoute.js";
 
-
-const app = express();
-
-dotenv.config({ path: "./config/config.env" });
-app.use(
-  cors({
-    origin: [process.env.FRONTEND_URL],
-    methods: ["GET"],
-    credentials: true,
-  })
-);
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use("./api/v1/reservation", reservationRouter);
-app.use("./api/v1/contact", contactRouter);
-app.get("/", (req, res, next)=>{return res.status(200).json({
-  success: true,
-  message: "HELLO WORLD AGAIN"
-})})
-
-dbConnection();
-
-app.use(errorMiddleware);
-
-export default app;
+import app from "../app.js"
+app.listen(process.env.PORT, ()=>{
+    console.log(`SERVER HAS STARTED AT PORT ${process.env.PORT}`);
+})
